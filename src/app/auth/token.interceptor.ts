337cdb1919a2acb176b7c3d2759b58ed9c5a210f
Mirza Jahic <mirzajahic1@hotmail.com>
2025-10-06 01:43:16 +0200

    Improving and testing

--- src/app/auth/token.interceptor.ts
+++ src/app/auth/token.interceptor.ts
@@ -3,10 +3,8 @@ import { HttpInterceptorFn } from '@angular/common/http';
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
   const token = sessionStorage.getItem('token');
-  console.log('Retrieved token from sessionStorage:', token);
   // Skip attaching token for login endpoint
   if (token && !req.url.endsWith('/login')) {
-    console.log('Attaching token to request:', token);
     req = req.clone({
       setHeaders: { Authorization: `Bearer ${token}` }
     });
